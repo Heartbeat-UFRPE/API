4e458a4761e22ef9a3fbb0a842d4cdcb58cd5e6a
Blizzard-Super <italoolavo@hotmail.com>
2020-09-15 16:40:57 -0300

    Update popularDB.js

--- src/popularDB.js
+++ src/popularDB.js
@@ -1,9 +1,10 @@
-const popularDataBase = `INSERT INTO Users VALUES 
+const popularDataBase = `INSERT INTO Users VALUES //Usuários para teste.
 (default,'João','joao@gmail.com','1985-04-23','M', 'jakdf8888'),
 (default,'Clara','clara@gmail.com','1990-07-10','F', '4K8811'),
 (default,'Cintia','cintia@gmail.com','1997-01-31','F','1111111'),
 (default,'Christian','christian@gmail.com','1989-11-30','M','22222222'),
 (default,'josé','josé@gmail.com','1978-02-23','M','121212121')`;
+(default,'Joaquim','jquim@gmail.com','1985-02-23','M','asfd#$&%')`;
 
 
 
